4104e2bddfe1db8164d61c589821ebb9f81abd42
N K MOHAMMED RANEESH <23br15442@rit.ac.in>
2026-05-19 23:45:34 +0530

    Update time_client.c

--- time_client.c
+++ time_client.c
@@ -1,34 +1,26 @@
-// gcc -o time_client time_client.c
-// ./time_client 127.0.0.1
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-
-int main(int c, char **v) {
-    if (c < 2) {
-        puts("Usage: ./time_client <server-ip>");
-        return 1;
-    }
-
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in srv = {AF_INET, htons(13000)};
-    inet_pton(AF_INET, v[1], &srv.sin_addr);
-
-    // Send time request
-    char *req = "TIME";
-    sendto(sock, req, strlen(req), 0, (void*)&srv, sizeof(srv));
-    printf("Time request sent to %s:13000\n", v[1]);
-
-    // Receive time response
-    char buf[256];
-    struct sockaddr_in from;
-    socklen_t fl = sizeof(from);
-    int n = recvfrom(sock, buf, 255, 0, (void*)&from, &fl);
-    if (n > 0) {
-        buf[n] = 0;
-        printf("Server Time: %s", buf);
-    }
-
-    close(sock);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+int main()
+{
+        int server_fd;
+        struct sockaddr_in server_addr,client_addr;
+        socklen_t len=sizeof(server_addr);
+        char buffer[1024];
+        int bytes;
+        char time;
+        server_fd=socket(AF_INET,SOCK_DGRAM,0);
+        server_addr.sin_family=AF_INET;
+        server_addr.sin_port=htons(8053);
+        server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+        memset(buffer,0,sizeof(buffer));
+        sendto(server_fd,&time,sizeof(time)-1,0,(struct sockaddr*)&server_addr,len);
+        bytes=recvfrom(server_fd,buffer,sizeof(buffer)-1,0,(struct sockaddr*)&server_addr,&len);
+        buffer[bytes]='\0';
+        printf("%s",buffer);
+        close(server_fd);
+        return 0;
 }
